a6dabff48a02ba813397159c70f56a3d85c0b4c3
Walter Barbieri <walter90@hotmail.it>
2023-08-25 17:14:24 +0200

    Aggiunta scrittura log allarmi su file esterno

--- w19Progetto/src/main/java/w19Progetto/centroControllo/ProxyCentroDiControllo.java
+++ w19Progetto/src/main/java/w19Progetto/centroControllo/ProxyCentroDiControllo.java
@@ -1,15 +1,31 @@
 package w19Progetto.centroControllo;
 
+import java.io.FileWriter;
+
 import w19Progetto.sonda.Sonda;
 
 public class ProxyCentroDiControllo {
 	private CentroDiControllo centroControllo;
+	private FileWriter fileWriter;
 
 	public ProxyCentroDiControllo(CentroDiControllo centroControllo) {
 		this.centroControllo = centroControllo;
+		try {
+			fileWriter = new FileWriter("alarmLog.txt", true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Finto database non trovato");
+		}
 	}
 
 	public void receiveAlarm(Sonda sonda) {
+		try {
+			fileWriter.write(sonda.toString() + "\n");
+			fileWriter.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Errore nella scrittura del finto database");
+		}
 		if (sonda.getLivelloFumo() > 5) {
 			centroControllo.sendAlarmToOperators(alarmUrl(sonda));
 		}

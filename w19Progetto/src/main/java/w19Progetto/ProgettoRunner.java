f48f85e71683826c17e561713725683e76424bdd
Walter Barbieri <walter90@hotmail.it>
2023-08-25 16:52:15 +0200

    aggiunte istanze, inizio i test

--- w19Progetto/src/main/java/w19Progetto/ProgettoRunner.java
+++ w19Progetto/src/main/java/w19Progetto/ProgettoRunner.java
@@ -27,9 +27,19 @@ public class ProgettoRunner implements CommandLineRunner {
 		CentroDiControllo controlCenter = new CentroDiControllo();
 		ProxyCentroDiControllo proxyControlCenter = new ProxyCentroDiControllo(controlCenter);
 		Sonda sonda1 = new Sonda(64.2, 187.4);
+		Sonda sonda2 = new Sonda(226.1, 1.9);
+		Sonda sonda3 = new Sonda(122.8, 45.7);
+		Sonda sonda4 = new Sonda(15.7, 342.4);
 		ProcessoDiControllo processo1 = processiFactory.installController(proxyControlCenter);
+		ProcessoDiControllo processo2 = processiFactory.installController(proxyControlCenter);
 		sonda1.setAlarmSubscriber((AlarmSubscriber) processo1);
+		sonda2.setAlarmSubscriber((AlarmSubscriber) processo1);
+		sonda3.setAlarmSubscriber((AlarmSubscriber) processo2);
+		sonda4.setAlarmSubscriber((AlarmSubscriber) processo2);
 		sonda1.sendSignal();
+		sonda2.sendSignal();
+		sonda3.sendSignal();
+		sonda4.sendSignal();
 
 	}
 
